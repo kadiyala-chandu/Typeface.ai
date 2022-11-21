5f47de747e07074844bad9b49da4457e0f4d024d
kadiyala-chandu <83367412+kadiyala-chandu@users.noreply.github.com>
2022-11-21 15:50:54 +0530

    Update soln1.py

--- soln1.py
+++ soln1.py
@@ -1,1 +1,13 @@
-print("hello")
+def coversionof3( n):
+    s=""
+    sign = '-' if n<0 else ''
+    n = abs(n)
+    if n < 3:
+        return str(n)
+        s = ''
+    while n != 0:
+        s = str(n%3) + s
+        n = n//3
+    return sign+s
+n=int(input())
+print(coversionof3(n))
